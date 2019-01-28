dd404337a31b1d8441146bc6078161b63dc55825
Oliver von Olnhausen <olivervonolnhausen@Olivers-MacBook-Pro.local>
2019-01-28 08:52:56 +0100

    Version nr

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Olnhausen Design Support
 Description: Support och anpassad kod för din hemsida från Olnhausen Design. Behövs för att hemsidan skall fungera.
-Version: 0.4
+Version: 0.5
 License: GPL
 Author: Olnhausen Design
 Author URI: http://www.olnhausendesign.se
