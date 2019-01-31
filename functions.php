0af5f01b602a28df0e13d2c0d7e5d98f51a5ad70
Oliver von Olnhausen <olivervonolnhausen@Olivers-MacBook-Pro.local>
2019-01-31 21:31:56 +0100

    Added optimizations

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Olnhausen Design Support
 Description: Support och anpassad kod för din hemsida från Olnhausen Design. Behövs för att hemsidan skall fungera.
-Version: 0.8
+Version: 0.9
 License: GPL
 Author: Olnhausen Design
 Author URI: http://www.olnhausendesign.se
@@ -196,4 +196,7 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 
 //Optional: Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('master');
+
+//Include optimizations
+require 'optimizations.php';
 ?>
